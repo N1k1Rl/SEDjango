ef10ddae1b1f97f9d2a554630cfc5ae6a0a0f170
Nikola Różycka <276075@student.pwr.edu.pl>
2024-12-04 03:10:59 +0100

    Part 3

--- SEList4/myapp/urls.py
+++ SEList4/myapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import hello_world
-
+from .views import ProductListView, ProductDetailView, ProductCreateView
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
+    path("user/products/", ProductListView.as_view(),
+                    name="product_list"),
+    path("user/products/<int:pk>/", ProductDetailView.as_view(),
+                    name="product_detail"),
+    path("user/products/new/", ProductCreateView.as_view(),
+                    name="product_create"),
 ]
