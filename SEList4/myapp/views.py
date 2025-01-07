b92911baea04fd24fa95cd31c6f440acef355de0
Nikola Różycka <276075@student.pwr.edu.pl>
2025-01-07 23:35:37 +0100

    List 8 Part 2

--- SEList4/myapp/views.py
+++ SEList4/myapp/views.py
@@ -6,20 +6,20 @@ from myapp.serializers import ProductSerializer
 from rest_framework import viewsets
 from .models import Product, Customer, Order
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .permissions import IsAdminOrReadOnly
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly, IsAdminUser]
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly, IsAdminUser]
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly, IsAdminUser]
 
