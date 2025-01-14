3897bd1a8f704bf45948ade4001fc258029d11cd
Nikola Różycka <276075@student.pwr.edu.pl>
2025-01-14 19:18:53 +0100

    Step5 Part3

--- SEList4/myapp/views.py
+++ SEList4/myapp/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from itertools import product
-
+from rest_framework.filters import SearchFilter
 from rest_framework import viewsets
 from myapp.serializers import ProductSerializer
 from rest_framework import viewsets
@@ -14,6 +14,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly, IsAdminUser]
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
